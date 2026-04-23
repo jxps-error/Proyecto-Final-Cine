e5cc54258e1e18a5ac9b945c70839b18a48829a9
alvatiezo <148808102+alvatiezo@users.noreply.github.com>
2026-04-22 20:26:36 -0600

    Create Usuario.java

---
+++ Cine/src/cine/Usuario.java
@@ -0,0 +1,33 @@
+package cine;
+
+public class Usuario {
+
+    private String nombre;
+    private String cedula;
+    private String pin;
+    private Rol rol;
+
+    public Usuario(String nombre, String cedula, String pin, Rol rol) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.pin = pin;
+        this.rol = rol;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+}
